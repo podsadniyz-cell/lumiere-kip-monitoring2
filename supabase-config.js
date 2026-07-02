b33211488d5cf54b0d6da0a0c79ef13974d2267e
podsadniyz-cell <podsadniyz@bk.ru>
2026-07-02 04:22:29 +0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+const SUPABASE_URL = 'https://jyfnzayshkeuaxddqhjj.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_eU0-K05BBuma2x4ty4nIbw_Uai0IK7m'; // замени на свой полный ключ
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
